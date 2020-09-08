4767dc869d22747446c9b4011fb6420ab21e7d09
KellyCristinaAires <k.woys@hotmail.com>
2020-09-07 21:30:41 -0300

    usuario

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace atividadeolamundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ola usuario eu me chamo kelly");
+        }
+    }
+}
